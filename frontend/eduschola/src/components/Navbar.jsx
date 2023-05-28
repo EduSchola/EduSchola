b365aa25f0f4d69458a3d9c6a08c34eaa4037c31
whoissegun <jojololadivine05@gmail.com>
2023-05-28 19:04:41 -0400

    Added styles for xl and 2xl screens

--- frontend/eduschola/src/components/Navbar.jsx
+++ frontend/eduschola/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 const Navbar = () => {
     return ( 
-        <header className=" w-full ">
-            <nav className="navbar w-full flex xl:pt-5 xl:px-5 items-center">
-                <div className="logo xl:w-12 xl:h-12 rounded-full bg-orange-500 xl:mr-[10%]"></div>
+        <header className=" w-full">
+            <nav className="navbar w-full flex xl:pt-5 xl:px-5 2xl:px-5 2xl:pt-5 items-center">
+                <div className="logo xl:w-12 xl:h-12 2xl:w-14 2xl:h-14 rounded-full bg-orange-500 xl:mr-[10%] 2xl:mr-[25%]"></div>
 
-                <div className="navbar__links flex gap-x-20 text-lg font-medium xl:mr-[15%]">
+                <div className="navbar__links flex xl:gap-x-20 2xl:gap-x-20 text-lg font-medium xl:mr-[15%] 2xl:mr-[25%]">
                     <Link href="/" className="navbar__links text-blue-500 underline">Home</Link>
                     <Link href="/" className="navbar__link">Features</Link>
                     <Link href="/" className="navbar__link">Pricing</Link>
@@ -17,7 +17,7 @@ const Navbar = () => {
                         Sign In
                     </button>
 
-                    <button className="navbar__btn border border-blue-500 text-blue-500 rounded-md xl:px-3 xl:py-1">
+                    <button className="navbar__btn border border-blue-500 text-blue-500 rounded-md xl:px-3 xl:py-1 2xl:px-3 2xl:py-1">
                         Get Started
                     </button>
                 </div>
