820272fce8ad64c663ce30da3b2de1698bb23545
whoissegun <jojololadivine05@gmail.com>
2023-06-01 22:27:19 -0400

    Addedmore components and cleaned up tailwind styling

--- frontend/eduschola/src/app/(Home)/page.js
+++ frontend/eduschola/src/app/(Home)/page.js
@@ -1,11 +1,13 @@
-import Image from 'next/image'
 import Hero from './Hero'
 import CompanyOverview from './CompanyOverview'
+import ContactUs from './ContactUs'
 export default function Home() {
+
   return (
     <div>
       <Hero />
       <CompanyOverview />
+      <ContactUs />
     </div>
   )
 }
