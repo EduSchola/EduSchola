41f115e7865bd34ebe212a4da5d791a6964630af
dannyy2000 <akinsanyadaniel665@gmail.com>
2023-06-13 00:58:33 -0700

    create school

--- backend/eduschola_project/edusch_app/serializers.py
+++ backend/eduschola_project/edusch_app/serializers.py
@@ -1,27 +1,39 @@
 from rest_framework import serializers
-from .models import Student, Parent, Instructor, User
+from .models import Student, Parent, Instructor, User, School
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
-        
+
+
 class StudentSerializer(serializers.ModelSerializer):
     user = UserSerializer()
+
     class Meta:
         model = Student
         fields = '__all__'
 
+
 class ParentSerializer(serializers.ModelSerializer):
     user = UserSerializer()
+
     class Meta:
         model = Parent
         fields = '__all__'
 
+
 class InstructorSerializer(serializers.ModelSerializer):
     user = UserSerializer()
+
     class Meta:
         model = Instructor
         fields = '__all__'
 
 
+class SchoolSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = School
+        fields = ['school_id', 'name', 'address', 'contact_information']
--- backend/eduschola_project/settings.py
+++ backend/eduschola_project/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'eduschdb',
         'USER': 'postgres',
-        'PASSWORD': '12345',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
